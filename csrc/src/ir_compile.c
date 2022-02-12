e62794a1c93b63c12213caa828dcd94d35e5b151
Emeka Nkurumeh <emekankurumeh@outlook.com>
2022-02-12 02:42:46 -0800

    got hello worlds compiling with new design
    * increased performance of interpreter
      ** branching seems to perform better than branchless loads
    * implemented slight optimization for tape operations
    * performance overall seems to respond badly to most attempted optimzations

--- csrc/src/ir_compile.c
+++ csrc/src/ir_compile.c
@@ -25,6 +25,7 @@ static const char *compile_result_str[] = {
     [COMPILE_OK] = "ok",
 };
 
+#if 0
 static const char *reg_names[] = {
     [REG_EAX] = "eax", [REG_ECX] = "ecx", [REG_EDX] = "edx", [REG_EBX] = "ebx",
     [REG_ESP] = "esp", [REG_EBP] = "ebp", [REG_ESI] = "esi", [REG_EDI] = "edi",
@@ -36,6 +37,7 @@ static const char *mod_rm_names[] = {
     [MODE_SIB_2] = "sib_2",
     [MODE_REG_DIRECT] = "direct",
 };
+#endif
 
 /* use inc for registers, add for mem locs. add is 1 uop less. */
 static inline compile_result emit_add_sub(compile_ctx *ctx, mod_rm_mode mode,
@@ -87,89 +89,69 @@ static inline compile_result emit_add_sub(compile_ctx *ctx, mod_rm_mode mode,
 }
 
 static inline compile_result emit_code_tape(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                            size_t *idx) {
-  return emit_add_sub(ctx, MODE_REG_DIRECT, ctx_ir->data[*idx]);
+                                            size_t idx) {
+  return emit_add_sub(ctx, MODE_REG_DIRECT, ctx_ir->data[idx]);
 }
 
 static inline compile_result emit_code_cell(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                            size_t *idx) {
-  return emit_add_sub(ctx, MODE_REG_INDIRECT, ctx_ir->data[*idx]);
+                                            size_t idx) {
+  return emit_add_sub(ctx, MODE_REG_INDIRECT, ctx_ir->data[idx]);
 }
 
 #define ctx_patch_code(ctx, idx, ...)                                          \
   vec_insertarr(ctx, idx, ((uint8_t[]){__VA_ARGS__}),                          \
                 macro_count_args(__VA_ARGS__))
 
-static inline compile_result emit_code_loop(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                            size_t *idx) {
-  ir_op_t op = ctx_ir->data[*idx];
-  if (op.arg > INT32_MAX || op.arg < INT32_MIN) {
-    return COMPILE_OPERAND_SIZE;
-  }
+static inline compile_result emit_code_loop_start(compile_ctx *ctx, ir_ctx *ir,
+                                                  size_t size) {
+  unused(ir);
 
   /* cmp (%SP_REG), $0 */
   ctx_push_code(ctx, 0x80, mod_rm(MODE_REG_INDIRECT, 0x7, SP_REG), 0x0);
 
-  if (op.kind == LOOP_END) {
-    return COMPILE_OK;
-  }
-
-  /* if (ctx_ir->data[op.arg].kind != LOOP_END || */
-  /*     ctx_ir->data[op.arg].arg != (int_t)*idx) { */
-  if (ctx_ir->data[op.arg].kind != LOOP_END) {
-    char buf[1024] = {0};
-    printf("[%ld] %s\n", *idx, ir_fmt_op(op, buf));
-    printf("[%ld] %s\n", op.arg - 1, ir_fmt_op(ctx_ir->data[op.arg - 1], buf));
-    printf("error 1\n");
-    return COMPILE_MALFORMED_LOOP;
-  }
-
-  /* encode loop body */
-  compile_ctx loop_code = {0};
-  vec_reserve(&loop_code, op.arg - *idx);
-  if (ir_ctx_compile(&loop_code, ctx_ir) != 0) {
-    printf("bad things\n");
-    return COMPILE_MALFORMED_LOOP;
-  }
-
-  /* plus 6 for the largest possible jumps (backwards and forwards) */
-  /* i don't like these hardcoded magic numbers either. */
-  if (loop_code.length + 12 > INT32_MAX)
-    return COMPILE_MALFORMED_LOOP;
-
-  /* rather than worrying about unhygenic macros, i store the value here */
-  uint_t loop_size = loop_code.length;
-
   /* check if we can do a short jump (accounting for 2 byte jump) */
-  if (loop_code.length + 2 <= INT8_MAX) {
-    /* jne imm8 (end of loop) */
-    ctx_push_code(&loop_code, 0x75, -(int8_t)(loop_size));
+  if (size + 2 <= INT8_MAX) {
     /* je imm8 (start of loop) */
-    ctx_push_code(ctx, 0x74, (int8_t)(loop_code.length));
+    ctx_push_code(ctx, 0x74, (int8_t)(size));
   } else {
-    /* check if we can still do a short jump backwards */
-    if (loop_size <= INT8_MAX) {
-      /* jne imm8 (end of loop) */
-      ctx_push_code(&loop_code, 0x75, -(int8_t)(loop_size));
-    } else {
-      /* jne imm16/imm32 */
-      ctx_push_code(&loop_code, 0x0F, 0x85); /* 0x0F is a prefix byte */
-      int32_t arg = -(int32_t)loop_size;
-      vec_push_as_bytes(&loop_code, &arg);
-    }
     /* je imm16/imm32 (start of loop) */
     ctx_push_code(ctx, 0x0F, 0x84); /* 0x0F is a prefix byte */
-    int arg = (int32_t)(loop_code.length);
+    int arg = (int32_t)(size);
     vec_push_as_bytes(ctx, &arg);
   }
 
-  /* copy the loop body to the main buffer */
-  vec_extend(ctx, &loop_code);
-
-  /* jump to the end of the loop since we compiled its body */
-  *idx = op.arg;
   return COMPILE_OK;
 }
+
+static inline compile_result emit_code_loop_end(compile_ctx *ctx, ir_ctx *ir,
+                                                size_t idx) {
+  unused(ir);
+  unused(idx);
+
+  /* cmp (%SP_REG), $0 */
+  ctx_push_code(ctx, 0x80, mod_rm(MODE_REG_INDIRECT, 0x7, SP_REG), 0x0);
+
+  /* we add 2 to account for encoding short jumps,
+   * or the prefix to a long jump */
+  int32_t offset = -(ctx->length + 2);
+
+  /* check if we can do a short jump (accounting for 2 byte jump) */
+  if (offset >= INT8_MIN) {
+    /* jne imm8 (end of loop) */
+    ctx_push_code(ctx, 0x75, (int8_t)(offset));
+  } else {
+    /* jne imm16/imm32 */
+    ctx_push_code(ctx, 0x0F, 0x85); /* 0x0F is a prefix byte */
+    int32_t arg = -(int32_t)(offset);
+    /* jumps are relative to end of instruction,
+     * so we must account for the size of the adress itself. */
+    arg -= sizeof(arg);
+    vec_push_as_bytes(ctx, &arg);
+  }
+
+  return COMPILE_OK;
+}
+
 static const reg_t syscall_arg_reg[] = {REG_EBX, REG_ECX, REG_EDX,
                                         REG_ESI, REG_EDI, REG_EBP};
 
@@ -184,8 +166,8 @@ static const reg_t syscall_arg_reg[] = {REG_EBX, REG_ECX, REG_EDX,
 
 /* yeah this is kinda broken. it need to mov from one reg to another */
 static inline compile_result emit_code_write(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                             size_t *idx) {
-  ir_op_t op = ctx_ir->data[*idx];
+                                             size_t idx) {
+  ir_op_t op = ctx_ir->data[idx];
   if (op.arg > INT32_MAX || op.arg < INT32_MIN) {
     return COMPILE_OPERAND_SIZE;
   }
@@ -216,8 +198,8 @@ static inline compile_result emit_code_write(compile_ctx *ctx, ir_ctx *ctx_ir,
 }
 
 static inline compile_result emit_code_read(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                            size_t *idx) {
-  ir_op_t op = ctx_ir->data[*idx];
+                                            size_t idx) {
+  ir_op_t op = ctx_ir->data[idx];
   if (op.arg > INT32_MAX || op.arg < INT32_MIN) {
     return COMPILE_OPERAND_SIZE;
   }
@@ -240,7 +222,7 @@ static inline compile_result emit_code_read(compile_ctx *ctx, ir_ctx *ctx_ir,
 }
 
 static inline compile_result emit_code_trap(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                            size_t *idx) {
+                                            size_t idx) {
   unused(ctx_ir);
   unused(idx);
   ctx_push_code(ctx, 0xcd, 0x03);
@@ -248,11 +230,12 @@ static inline compile_result emit_code_trap(compile_ctx *ctx, ir_ctx *ctx_ir,
 }
 
 static inline compile_result emit_code_exit(compile_ctx *ctx, ir_ctx *ctx_ir,
-                                            size_t *idx) {
-  ir_op_t op = ctx_ir->data[*idx];
+                                            size_t idx) {
+  ir_op_t op = ctx_ir->data[idx];
   if (op.arg > INT32_MAX || op.arg < INT32_MIN) {
     return COMPILE_OPERAND_SIZE;
   }
+
   /* mov SYS_EXIT, %eax */
   ctx_push_code(ctx, 0xb8 + REG_EAX, SYS_EXIT, 0x0, 0x0, 0x0);
   int32_t arg = op.arg;
@@ -262,28 +245,58 @@ static inline compile_result emit_code_exit(compile_ctx *ctx, ir_ctx *ctx_ir,
   return COMPILE_OK;
 }
 
-size_t ir_ctx_compile(compile_ctx *ctx, ir_ctx *ctx_ir) {
-  static compile_result (*code_fn[])(compile_ctx *, ir_ctx *, size_t *) = {
-      [IR_OP_TAPE] = emit_code_tape,  [IR_OP_CELL] = emit_code_cell,
-      [IR_OP_LOOP] = emit_code_loop,  [IR_OP_WRITE] = emit_code_write,
-      [IR_OP_READ] = emit_code_read,  [IR_OP_SET] = emit_code_trap,
-      [IR_OP_PATCH] = emit_code_trap, [IR_OP_MAX] = emit_code_exit,
-  };
-  /* patch code to have a halt instruction */
-  vec_push(ctx_ir, ((ir_op_t){.kind = IR_OP_MAX, .arg = 0}));
+/* lifted outside the function to avoid stack overflow */
+char err_buf[500];
 
-  char err_buf[500];
+/* lifter outside since there is no reason to have this
+ * inside the function body */
+static compile_result (*code_fn[])(compile_ctx *, ir_ctx *, size_t) = {
+    [IR_OP_TAPE] = emit_code_tape,
+    [IR_OP_CELL] = emit_code_cell,
+    [IR_OP_LOOP_START] = emit_code_loop_start,
+    [IR_OP_LOOP_END] = emit_code_loop_end,
+    [IR_OP_WRITE] = emit_code_write,
+    [IR_OP_READ] = emit_code_read,
+    [IR_OP_SET] = emit_code_trap,
+    [IR_OP_MAX] = emit_code_exit,
+};
+
+size_t ir_ctx_compile__(compile_ctx *ctx, ir_ctx *ctx_ir, size_t idx) {
   compile_result err = COMPILE_OK;
 
-  for (size_t i = 0; i < ctx_ir->length; i++) {
-    /* remove LOOP flag */
-    if ((err = code_fn[ctx_ir->data[i].kind & IR_OP_MAX](ctx, ctx_ir, &i) !=
-               COMPILE_OK)) {
-      ir_fmt_op(ctx_ir->data[i], err_buf);
+  for (size_t i = idx; i < ctx_ir->length; i++) {
+    ir_op_t opcode = ctx_ir->data[i];
+    if (opcode.kind == IR_OP_LOOP_START) {
+      compile_ctx loop_code = {0};
+      /* an OK approximation maybe? */
+      vec_reserve(&loop_code, ctx_ir->data[i].arg * 2);
+      err = ir_ctx_compile__(&loop_code, ctx_ir, i + 1);
+      if (err == COMPILE_OK) {
+        err = emit_code_loop_start(ctx, ctx_ir, loop_code.length);
+        vec_extend(ctx, &loop_code);
+        /* skip to end of loop body */
+        i += opcode.arg;
+      }
+    } else {
+      err = code_fn[opcode.kind](ctx, ctx_ir, i);
+    }
+
+    if (err != COMPILE_OK) {
+      ir_fmt_op(opcode, err_buf);
       fprintf(stderr, "compiler error: [%s] %s\n", err_buf,
               compile_result_str[err]);
-      return -1;
+      return err;
+    }
+
+    if (opcode.kind == IR_OP_LOOP_END) {
+      break;
     }
   }
-  return 0;
+  return COMPILE_OK;
+}
+
+inline size_t ir_ctx_compile(compile_ctx *ctx, ir_ctx *ctx_ir) {
+  /* patch code to have a halt instruction */
+  vec_push(ctx_ir, ((ir_op_t){.kind = IR_OP_MAX, .arg = 0}));
+  return ir_ctx_compile__(ctx, ctx_ir, 0) == COMPILE_OK;
 }
--- csrc/src/ir_interpret.c
+++ csrc/src/ir_interpret.c
@@ -6,8 +6,14 @@ size_t ir_interpret(ir_ctx *ir_ctx, interpret_ctx_t *ctx) {
   /* patch code to have a halt instruction */
   vec_push(ir_ctx, ((ir_op_t){.kind = IR_OP_MAX, .arg = 0}));
   static void *dispatch_table[] = {
-      &&ir_op_tape, &&ir_op_cell, &&ir_op_loop,  &&ir_op_write,
-      &&ir_op_read, &&ir_op_set,  &&ir_op_patch, &&ir_op_halt,
+      [IR_OP_TAPE] = &&ir_op_tape,
+      [IR_OP_CELL] = &&ir_op_cell,
+      [IR_OP_LOOP_START] = &&ir_op_loop_start,
+      [IR_OP_LOOP_END] = &&ir_op_loop_end,
+      [IR_OP_WRITE] = &&ir_op_write,
+      [IR_OP_READ] = &&ir_op_read,
+      [IR_OP_SET] = &&ir_op_set,
+      [IR_OP_MAX] = &&ir_op_halt,
   };
   size_t ip = 0;
   size_t sp = 0;
@@ -25,14 +31,28 @@ size_t ir_interpret(ir_ctx *ir_ctx, interpret_ctx_t *ctx) {
   }
 
   goto *dispatch_table[opcode(ip).kind];
-  dispatch(ir_op_tape, { sp = (sp + opcode(ip).arg) % ctx->capacity; });
+  /* really bad. we need to optimize this */
+  /* dispatch(ir_op_tape, { sp = (sp + opcode(ip).arg) % ctx->capacity; }); */
+  dispatch(ir_op_tape, {
+    sp += opcode(ip).arg;
+    if (sp >= ctx->capacity) {
+      sp -= ctx->capacity;
+    }
+  });
   dispatch(ir_op_cell, { ctx->data[sp] += opcode(ip).arg; });
-  dispatch(ir_op_loop, {
-    int64_t target = opcode(ip).arg;
-    if (!ctx->data[sp] && opcode(ip).kind == (ir_op_kind_t)LOOP_START) {
-      ip = target;
-    } else if (ctx->data[sp] && opcode(ip).kind == (ir_op_kind_t)LOOP_END) {
-      ip = target;
+  /* branchless loads perform much worse */
+  dispatch(ir_op_loop_start, {
+    int64_t delta = opcode(ip).arg;
+    /* ip += (ctx->data[sp] == 0) * delta; */
+    if (!ctx->data[sp]) {
+      ip += delta;
+    }
+  });
+  dispatch(ir_op_loop_end, {
+    int64_t delta = opcode(ip).arg;
+    /* ip += (ctx->data[sp] != 0) * delta; */
+    if (ctx->data[sp]) {
+      ip += delta;
     }
   });
   dispatch(ir_op_write, {
@@ -46,10 +66,6 @@ size_t ir_interpret(ir_ctx *ir_ctx, interpret_ctx_t *ctx) {
     }
   });
   dispatch(ir_op_set, { ctx->data[sp] = opcode(ip).arg; });
-  dispatch(ir_op_patch, {
-    fputs("invalid instruction in stream", stderr);
-    return 1;
-  });
   dispatch(ir_op_halt, {
     fflush(stdout);
     return 0;
